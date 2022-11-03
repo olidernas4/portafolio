b740dc0190e149e93a69524de6563da9304a0523
Your Name <you@example.com>
2022-11-03 17:06:49 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Header from './components/header/Header';
+
+const App =() =>{
+  return(
+
+    <>
+    <Header/>
+    
+    
+    
+    </>
+  )
+}
+
+export default App;
